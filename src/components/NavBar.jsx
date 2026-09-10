e26eac40c1209ae9239bee218ec4bc59ab0628c0
Harish3005-cloud <eharish3005@gmail.com>
2026-09-10 23:18:51 +0530

    resume-updation

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -36,7 +36,7 @@ const NavBar = () => {
             <span>Contact Me</span>
           </div>
         </a>
-        <a href="/Resume/Hari_1_Resume.pdf" target="_blank" rel="noopener noreferrer" className="contact-btn group">
+        <a href="/Resume/Hari_2_Resume.pdf" target="_blank" rel="noopener noreferrer" className="contact-btn group">
           <div className="inner">
             <span>Resume</span>
           </div>
